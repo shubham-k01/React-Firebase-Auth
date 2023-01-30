059a13761129dc6d17ca006450354e20e877f133
shubham-k01 <skcode123@gmail.com>
2023-01-30 11:01:47 +0530

    Completed

--- src/firebase.js
+++ src/firebase.js
@@ -6,22 +6,14 @@ require("firebase/auth")
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER,
-//   appId: process.env.REACT_APP_FIREBASE_APP
-// };
 const firebaseConfig = {
-    apiKey: "AIzaSyDYGxTNaXkIjJW9ulvkHhBBtSV5j08vBvk",
-    authDomain: "react-auth-dev-d7479.firebaseapp.com",
-    projectId: "react-auth-dev-d7479",
-    storageBucket: "react-auth-dev-d7479.appspot.com",
-    messagingSenderId: "602533633055",
-    appId: "1:602533633055:web:41eaeef348f95aaccf092a"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER,
+  appId: process.env.REACT_APP_FIREBASE_APP
+};
 
 
 // Initialize Firebase
